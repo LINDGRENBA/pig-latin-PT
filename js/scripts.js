81c9f4714e20060098eff2e4c56e17b5ef751728
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-16 11:52:23 -0700

    Add business logic to move 'qu' to end of string and add 'ay'.

--- js/scripts.js
+++ js/scripts.js
@@ -8,28 +8,13 @@ let pigLatin = function(userInput) {
   if(userInput.length === 1 && firstVowelPosition === 0) {
     return userInput + 'way';
   } else if(firstVowelPosition === 0) {
-    return userInput + 'yay'
+    return userInput + 'yay';
+  } else if (userInput.charAt(0) === 'q' && userInput.charAt(1) === 'u') {
+    return userInput.slice(2) + userInput.slice(0, 2) + 'ay';
+  } else {
+    return userInput.slice(firstVowelPosition) + userInput.slice(0, firstVowelPosition) + 'ay'
   }
-
-
-
-
-
-
-
-  // if the length of the user input is one /array.length = 1 / then do the thing below
-  // let vowels = ['a', 'i', 'u', 'e', 'o'];
-  // may want a consonants array as well as an empty array to hold the characters of the words the user passed in
-//   let newWord;
-//   vowels.forEach(function(vowel) {
-//     if(userInput === vowel) {
-//       newWord = userInput + 'way';
-//     }
-//   });
-//   return newWord; 
 }
-
-
 
 // user interface
 $(document).ready(function() {
@@ -44,3 +29,16 @@ $(document).ready(function() {
 
   });
 });
+
+
+// original business logic, discarded at time of refactoring
+  // if the length of the user input is one /array.length = 1 / then do the thing below
+  // let vowels = ['a', 'i', 'u', 'e', 'o'];
+  // may want a consonants array as well as an empty array to hold the characters of the words the user passed in
+//   let newWord;
+//   vowels.forEach(function(vowel) {
+//     if(userInput === vowel) {
+//       newWord = userInput + 'way';
+//     }
+//   });
+//   return newWord; 
